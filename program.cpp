023512997c91e89e4e03588dc7b2157860f77e9a
georgieva07 <georgieva07@users.noreply.github.com>
2019-04-16 15:45:12 +0200

    Functions implemented

--- program.cpp
+++ program.cpp
@@ -4,10 +4,12 @@ using namespace std;
 
 void printMyName () {
 	//TODO
+	cout << "Anastasija Gjeorgieva" << endl;
 }
 
 void printMyIndex () {
 	//TODO
+	cout << "171045" << endl;
 }
 
 int main () {
